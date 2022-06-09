e850ba37064d5124f085f3d552759943caebcf4a
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-06-09 15:27:46 -0700

    Added team manager prompt array

---
+++ index.js
@@ -0,0 +1,58 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const teamManager = [
+    {
+        type: "input",
+        name: "managerName",
+        message: "Enter the team manager's name.",
+        validate: async (input) => {
+            if (input) {
+                return true;
+            } else {
+                console.log("Project manager required. Please enter a name.");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "Enter the manager's email.",
+        validate: async (input) => {
+            if (input) {
+                return true;
+            } else {
+                console.log("Manager email required");
+                return false;
+            }
+        }
+    }, 
+    {
+        type: "input", 
+        name: "officeNumber", 
+        message: "Enter the manager's office number.",
+        validate: async (input) => {
+            if (input) {
+                return true;
+            } else {
+                console.log("Manager office number required.");
+                return false;
+            }
+        }
+    }, 
+    {
+        type: "checkbox", 
+        name: "teamAdditions", 
+        message: "Would you like to add a team member?", 
+        choices: ["Engineer", "Intern", "None"], 
+        validate: async (checkbox)  => {
+            if (checkbox.length == 1) {
+                return true; 
+            } else {
+                console.log("Please choose one option."); 
+                return false; 
+            }
+        }
+    }
+]
